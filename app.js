31f90d30f658687cf57445fa362ebe725be3fdff
prem kumar <imnotpremkumar@gmail.com>
2024-12-10 01:08:22 +0530

    bcrypt

--- app.js
+++ app.js
@@ -1,57 +1,63 @@
 import dotenv from "dotenv";
-import express from "express" ;
+import express from "express";
 import ejs from "ejs";
 import bodyParser from "body-parser"
 import mongoose from "mongoose";
 import { User } from "./models/user.js";
 import md5 from "md5";
+import bcrypt from "bcrypt"
 
 dotenv.config();
 const app = express();
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.set("view engine","ejs")
+app.set("view engine", "ejs")
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
-app.get("/",function(req,res){
+const saltRounds = 10;
+
+app.get("/", function (req, res) {
   res.render("home")
 })
 
-app.get("/login",function(req,res){
+app.get("/login", function (req, res) {
   res.render("login")
 })
 
-app.get("/register",function(req,res){
+app.get("/register", function (req, res) {
   res.render("register")
 })
 
-app.post("/register",async function(req,res){
-  const user = new User({
-    email : req.body.username,
-    password : md5(req.body.password)
+app.post("/register", async function (req, res) {
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    const user = new User({
+      email: req.body.username,
+      password: hash
+    })
+    try {
+      user.save();
+      res.render("secrets")
+    } catch (err) {
+      res.send("please Enter your Email and Password");
+    }
   })
+});
 
-  // user.save();
 
-  try {
-    user.save();
-    res.render("secrets")
-  } catch (err) {
-    res.send("please Enter your Email and Password");
+app.post("/login", async function (req, res) {
+  let foundUser = await User.findOne({ email: req.body.username })
+  if (foundUser) {
+    bcrypt.compare(req.body.password, foundUser.password, function (err, result) {
+      if(result==true){
+        res.render("secrets");
+      }
+    });
+    
   }
+
 })
 
-app.post("/login",async function(req,res){
-  let foundUser = await User.findOne({email : req.body.username})
-  if (foundUser.password === md5(req.body.password)){
-    res.render("secrets");
-  } else {
-    res.render("login");
-  }
-  
-})
-
-app.listen(3000,function(){
+app.listen(3000, function () {
   console.log("sever is started on port 3000");
 })
