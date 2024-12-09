72fb65cdb4fc1d530bff583935a8eca4d72ed342
prem kumar <imnotpremkumar@gmail.com>
2024-12-10 00:19:02 +0530

    encryption

---
+++ app.js
@@ -0,0 +1,56 @@
+import dotenv from "dotenv";
+import express from "express" ;
+import ejs from "ejs";
+import bodyParser from "body-parser"
+import mongoose from "mongoose";
+import { User } from "./models/user.js";
+
+dotenv.config();
+const app = express();
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static("public"));
+app.set("view engine","ejs")
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+app.get("/",function(req,res){
+  res.render("home")
+})
+
+app.get("/login",function(req,res){
+  res.render("login")
+})
+
+app.get("/register",function(req,res){
+  res.render("register")
+})
+
+app.post("/register",async function(req,res){
+  const user = new User({
+    email : req.body.username,
+    password : req.body.password
+  })
+
+  // user.save();
+
+  try {
+    user.save();
+    res.render("secrets")
+  } catch (err) {
+    res.send("please Enter your Email and Password");
+  }
+})
+
+app.post("/login",async function(req,res){
+  let foundUser = await User.findOne({email : req.body.username})
+  if (foundUser.password === req.body.password){
+    res.render("secrets");
+  } else {
+    res.render("login");
+  }
+  
+})
+
+app.listen(3000,function(){
+  console.log("sever is started on port 3000");
+})
---
+++ models/user.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import encrypt from "mongoose-encryption";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const userSchema = new mongoose.Schema({
+
+  email : {
+    type : String,
+  } ,
+  password : {
+    type : String,
+    required : [true,"A password is required"]
+  } 
+})
+
+let secret = process.env.SECRET ;
+
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+
+//userSchema.plugin(encrypt,{secret : secret , encryptedFieds : ["password"]});
+
+export const User = new mongoose.model("User",userSchema) ;
